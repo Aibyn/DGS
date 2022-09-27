f2a822ba189f277665f46fb209b2e27f47f5eb89
Aibyn Khaireden <aibyn.h.1@gmail.com>
2022-09-27 17:15:00 +0600

    initial commit

---
+++ src/main/java/aibyn/product/mutations/ProductDataMutation.java
@@ -0,0 +1,26 @@
+package aibyn.product.mutations;
+
+import aibyn.product.repository.ProductRepository;
+import com.netflix.dgs.codegen.generated.types.InputProduct;
+import com.netflix.dgs.codegen.generated.types.Product;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsData;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.InputArgument;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@DgsComponent
+public class ProductDataMutation {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @DgsMutation
+    public List<Product> addProduct(@InputArgument InputProduct product) {
+        productRepository.add(product);
+        return productRepository.getProducts();
+
+    }
+}
---
+++ src/main/java/aibyn/product/repository/ProductRepository.java
@@ -0,0 +1,30 @@
+package aibyn.product.repository;
+
+import aibyn.product.util.ProductUtil;
+import com.netflix.dgs.codegen.generated.types.InputProduct;
+import com.netflix.dgs.codegen.generated.types.Product;
+import com.netflix.dgs.codegen.generated.types.ProductType;
+import graphql.com.google.common.collect.Lists;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductRepository {
+    private List<Product> products = Lists.newArrayList(
+            Product.newBuilder().type(ProductType.WEIGHT).name("Cucumber").price(100).build(),
+            Product.newBuilder().type(ProductType.WEIGHT).name("Tomato").price(50).build(),
+            Product.newBuilder().type(ProductType.SINGLE).name("Coca Cola 0.5l").price(200).build(),
+            Product.newBuilder().type(ProductType.SINGLE).name("Fanta 0.5l").price(300).build(),
+            Product.newBuilder().type(ProductType.PACKAGE).name("6 x Coca Cola 0.5l").price(200).build(),
+            Product.newBuilder().type(ProductType.PACKAGE).name("6 x Fanta 0.5l").price(300).build()
+    );
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void add(InputProduct product) {
+        products.add(ProductUtil.convertInput(product));
+    }
+}
